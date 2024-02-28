7e52e5c6f4a1bfb857c3d73075b67ab8e70a769e
AykhanTa <aykhanft@code.edu.az>
2024-02-28 13:29:57 +0400

    Add project files.

---
+++ DateTimeHomeTask/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DateTimeHomeTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseArray(1,2,3,4,5);
+        }
+
+        static void ReverseArray(params int[] arr)
+        {
+            int[] result = new int[arr.Length];
+            int j = 0;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                result[j] = arr[i];
+                j++;
+            }
+            foreach (var item in result)
+            {
+                Console.Write(item+" ");
+            }
+        }
+    }
+}
